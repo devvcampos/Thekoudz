15f0dd22b3430dab77421b1e560474e6cc320744
Pedro Campos <90489838+devvcampos@users.noreply.github.com>
2026-08-14 00:50:47 -0300

    Update config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+local Config = {
+    MaxESP_Dist = 150,
+
+    ESP = {
+        Enabled = false,
+        DrawBox = true,
+        DrawName = true,
+        DrawHealth = true,
+        DrawDistance = true,
+        BoxColor = Color3.new(1, 1, 1)
+    }
+}
+
+return Config
